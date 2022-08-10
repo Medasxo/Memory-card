d70db56c8230079744a4494ae15b85319821f0e5
Medas <medas02345@gmail.com>
2022-08-10 12:02:10 +0300

    Import pictures of the drivers for the cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import Header from "./Header";
 import "./App.css";
-import { Component, react} from "react";
+import { Component} from "react";
+
+import albon from "./driverPhotos/albon.png";
+import alonso from "./driverPhotos/alonso.png";
+import bottas from "./driverPhotos/bottas.png";
+import checo from "./driverPhotos/checo.png";
+import daniel from "./driverPhotos/daniel.png";
+import gasly from "./driverPhotos/gasly.png";
+import george from "./driverPhotos/george.png";
+import hamilton from "./driverPhotos/hamilton.png";
+import lance from "./driverPhotos/lance.png";
+import latifi from "./driverPhotos/latifi.png";
+import leclerc from "./driverPhotos/leclerc.png";
+import magnussen from "./driverPhotos/magnussen.png";
+import max from "./driverPhotos/max.png";
+import mick from "./driverPhotos/mick.png";
+import ocon from "./driverPhotos/ocon.png";
+import sainz from "./driverPhotos/sainz.png";
+import tsunoda from "./driverPhotos/tsunoda.png";
+import vettel from "./driverPhotos/vettel.png";
+import zhou from "./driverPhotos/zhou.png";
+
+
+
 
 class App extends Component{
+  constructor(){
+    super();
+    this.displayCards = this.displayCards.bind(this);
+  }
+
+  displayCards(){
+      return (
+        <div className="card">
+          <img src={albon}></img>
+        </div>
+        
+    
+      );
+    
+  }
   render() {
     return (
       <div className="App">
         <Header />
+        <div className="cardContainer">
+          {this.displayCards()}
+        </div>
       </div>
     );
   }
