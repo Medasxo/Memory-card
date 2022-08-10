790b85febb754f895e6287df77bb6eecccd1649c
Medas <medas02345@gmail.com>
2022-08-10 21:44:18 +0300

    Add all the driver names and images into the array

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from "./Header";
 import "./App.css";
-import { Component} from "react";
+import { Component } from "react";
 
 import albon from "./driverPhotos/albon.png";
 import alonso from "./driverPhotos/alonso.png";
@@ -22,18 +22,33 @@ import tsunoda from "./driverPhotos/tsunoda.png";
 import vettel from "./driverPhotos/vettel.png";
 import zhou from "./driverPhotos/zhou.png";
 
-
-
-
-const App = () =>{
+const App = () => {
+  const drivers = [
+    { name: "Pierre Gasly", image: gasly },
+    { name: "George Russel", image: george },
+    { name: "Mick Schumacher", image: mick },
+    { name: "Max Verstappen", image: max },
+    { name: "Charles Leclerc", image: leclerc },
+    { name: "Kevin Magnussen", image: magnussen },
+    { name: "Esteban Ocon", image: ocon },
+    { name: "Carlos Sainz", image: sainz },
+    { name: "Yuki Tsunoda", image: tsunoda },
+    { name: "Sebastian Vettel", image: vettel },
+    { name: "Zhou Ghuanyu", image: zhou },
+    { name: "Nicholas Latifi", image: latifi },
+    { name: "Lance Stroll", image: lance },
+    { name: "Lewis Hamilton", image: hamilton },
+    { name: "Daniel Riccardo", image: daniel },
+    { name: "Sergio Perez", image: checo },
+    { name: "Valteri Bottas", image: bottas },
+    { name: "Fernando Alonso", image: alonso },
+    { name: "Alexander Albon", image: albon },
+  ];
   return (
     <div className="App">
       <Header />
-      <div className="cardContainer">
-  
-        
-      </div>
+      <div className="cardContainer"></div>
     </div>
   );
-}
+};
 export default App;
